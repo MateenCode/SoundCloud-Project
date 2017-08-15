7949b6bc892c1dd83686eb70f20715a8173088b2
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-08-14 21:22:52 -0500

    Updated fixed and ready too ship

--- main.js
+++ main.js
@@ -1,59 +1,87 @@
-// 1. First select ad store the elements you'll be working with
-let display = document.querySelector('#display')
-let textValue = document.querySelector('#TextValue')
-let token = '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q'
+// Element im working with
+let display = document.querySelector('#display');
+let textValue = document.querySelector('#TextValue');
+let searchLogo = document.querySelector('.searchLogo');
+let token = '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=';
 
-// 2. Create your `keydown` event for getting the user's search term
 
+
+// key down event for enter, pass down ID value
   textValue.addEventListener('keydown', function(event){
 
   if(event.keyCode === 13){
+
+    display.textContent = ""
+    
+    event.preventDefault();
+
     return(getInfo(event.target.value));
   }
 
+
 });
 
-// 3. Create your `fetch` request that is called after a submission
-getInfo("")
 
+// fetch API pass down artist event and stores users ID
 function getInfo(event){
-fetch(`https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=${event}`) //API address used to grab info
+fetch(`http://api.soundcloud.com/users/${token}${event}`) //API address used to grab info
       .then( function(response) {
         return response.json() //returns API in Json format
       }).then(function(data){
-              moreInfo(data)
-      })
+
+        let userID = data[0].id;
+
+        TrackInfo(userID);
+
+      });
+};
+
+
+// Another Fetch request that uses the ID to search for unique track
+
+
+function TrackInfo(userID){
+
+  fetch(`http://api.soundcloud.com/users/${userID}/tracks/${token}`)
+    .then( function(response){
+      return response.json()
+    })
+    .then( function(data){
+      moreInfo(data)
+    })
 }
 
 
 
 
-// 4. Create a way to append the fetch results to your page
-
+// fill in dynamic html song-cards with fetch values
 
 function moreInfo(data){
   displayResults="";
   for(i=0; i<data.length; i++){
 
-    var songLink = `${data[i].stream_url}`
+    let id = data[i].id
 
-    displayResults += `<div class="w3-card-4">
-                  <img class="albumArt" src="${data[i].artwork_url}"/>
+    displayResults += `
+                  <div class="w3-card-4"  type="click" value=${id} id=${id} onclick=audio(${id})>
+                  <img class="albumArt" data-song-link="${data[i].stream_url}" src="${data[i].artwork_url}"/>
                   <p> <b>Title: </b>  ${data[i].title}</p>
                   <p> <b>Artist: </b>  ${data[i].user.username}</p>
                   </div>
-
-                  <div class="music-container">
-                    <audio class="music-player" controls="controls" src="${songLink}${token}"></audio>
-                  </div>
-
                   `
   }
-   display.innerHTML = displayResults
+
+   display.insertAdjacentHTML('afterbegin', displayResults);
 }
 
 
 
+// querySelect audio tag give src attribute that adds + stream link API
 
+function audio(id){
 
-// 5. Create a way to listen for a click that will play the song in the audio play
+  let audioSource = document.querySelector("audio");
+
+  audioSource.src = "https://api.soundcloud.com/tracks/"+id+"/stream?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f"
+
+}
